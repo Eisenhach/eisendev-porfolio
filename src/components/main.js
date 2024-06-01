27c2aff653da301ff6e71df1327104b75bd7bdc3
Tomas Eisenhach Pablo <eisenhach.tomas@gmail.com>
2024-05-31 21:48:42 -0300

    prueba

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  offset: 120, // Desplazamiento (en píxeles) desde el borde inferior del viewport hasta el elemento
+  delay: 0, // Retraso en milisegundos antes de que se inicie la animación
+  duration: 400, // Duración de la animación en milisegundos
+  easing: "ease", // Función de aceleración utilizada
+  once: false, // Si la animación debe ocurrir solo una vez
+  mirror: true,
+  anchorPlacement: "top-bottom",
+});
