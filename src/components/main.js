229bfb1c30cd341b4210dd0a27907fe7a270cfb2
Tomas Eisenhach Pablo <eisenhach.tomas@gmail.com>
2024-06-01 10:38:18 -0300

    pruebas

--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 AOS.init({
+  startEvent: "DOMContentLoaded",
   offset: 120, // Desplazamiento (en píxeles) desde el borde inferior del viewport hasta el elemento
   delay: 0, // Retraso en milisegundos antes de que se inicie la animación
   duration: 400, // Duración de la animación en milisegundos
